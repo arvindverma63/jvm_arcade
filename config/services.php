cf8a0f0e21aa8ca88b274b6c0f0ed31d4d4b5773
arvindverma63 <arvindverma630635@gmail.com>
2025-12-21 17:30:21 +0530

    update

--- config/services.php
+++ config/services.php
@@ -34,13 +34,13 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/github/callback',
+        'redirect' => env('GITHUB_REDIRECT_URL'),
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' =>   'http://127.0.0.1:8000/auth/google/callback',
+        'redirect' =>   env('GOOGLE_REDIRECT_URL'),
     ],
 
 ];
